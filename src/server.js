b1134abc065a48b22688f4106d6f9a0cef62a1f9
LeOzInNDF <leo_albano@hotmail.com>
2023-03-10 00:43:15 -0300

    Update Code

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require("./app")
 const port = process.env.PORT || 5000
 
 app.listen(port, () =>{
-  console.log(`Server listening on port 5000 ${port}`)
+  console.log(`Server listening on port ${port}`)
 })
