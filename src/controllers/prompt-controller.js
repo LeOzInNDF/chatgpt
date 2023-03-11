ef4df22179c31dc02a06aa52592034a259fd5c50
LeOzInNDF <leo_albano@hotmail.com>
2023-03-10 23:57:13 -0300

    Create Code

--- src/controllers/prompt-controller.js
+++
@@ -1,9 +0,0 @@
-
-module.exports ={
-
-  async sendText(request, response) {
-      return response.status(200).json({
-        message: "Caio castro chegou no rolê"
-      })
-  }
-}
--- src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const promptController = require("../controllers/prompt-controller")
-
-const routes = express.Router()
-
-routes.post('/api/prompt', promptController.sendText)
-module.exports = routes
-
-// http://localhost:5001/api/prompt
